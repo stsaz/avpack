1df3f825e6cb233ed38a1e79ce6f91689e9a56c0
Simon Zolin <4729655+stsaz@users.noreply.github.com>
2025-06-07 09:06:17 +0300

    minor

--- avpack/base/ts.h
+++ avpack/base/ts.h
@@ -3,6 +3,9 @@
 
 /*
 ts_pkt_read
+ts_top_read
+ts_info_read
+ts_data_hdr_read
 */
 
 /* Format:
@@ -19,7 +22,6 @@ struct ts_packet {
 	ffuint pid, counter;
 	ffuint start;
 	ffuint adaptation_len, adaptation_flags;
-	ffuint64 pos_msec;
 	ffuint64 off;
 	ffstr body;
 };
@@ -27,6 +29,21 @@ struct ts_packet {
 enum TS_STREAM {
 	TS_STREAM_AUDIO_MP3 = 3,
 	TS_STREAM_AUDIO_AAC = 15,
+};
+
+enum _TSR_PMT {
+	_TSR_PMT_TOP,
+	_TSR_PMT_INFO,
+	_TSR_PMT_DATA,
+};
+
+struct _tsr_pm {
+	ffuint pid;
+	ffuint type; // enum _TSR_PMT
+	ffuint stream_type; // enum TS_STREAM
+	ffuint stream_id;
+	ffuint pkt_len;
+	ffuint64 pos_msec;
 };
 
 static ffuint ts_read8(const ffbyte **d, const ffbyte *end)
@@ -83,4 +100,96 @@ static inline int ts_pkt_read(struct ts_packet *p, const void *data, size_t len)
 		ffstr_set(&p->body, d, end - d);
 
 	return len;
+}
+
+/* Format:
+[8]
+sid[2]
+pid_info[2]
+*/
+static inline int ts_top_read(ffstr data)
+{
+	const ffbyte *d = (ffbyte*)data.ptr;
+	d += 8;
+	d += 2;
+
+	ffuint pid = ffint_be_cpu16_ptr(d) & 0x1fff;
+	d += 2;
+
+	return pid;
+}
+
+/* Format:
+[1]
+[2]
+sid[2]
+[3]
+pid_data[2]
+len[2]
+	...
+stream_type
+pid[2]
+desc_list_len[2]
+tags {
+	desc_tag
+	len
+	data[]
+}...
+*/
+static inline int ts_info_read(ffstr data, ffuint *stream_type)
+{
+	const ffbyte *d = (ffbyte*)data.ptr;
+	d += 1;
+	d += 2;
+	d += 2;
+	d += 3;
+
+	ffuint pid = ffint_be_cpu16_ptr(d) & 0x1fff;
+	d += 2;
+
+	ffuint n = ffint_be_cpu16_ptr(d) & 0x0fff;
+	if (n != 0)
+		return 0; // case not supported
+	d += 2;
+
+	*stream_type = *d++;
+	return pid;
+}
+
+/* Format:
+header[2] // 0x0001
+stream_id
+packet_len[2]
+[1]
+flags
+pos_len
+pos[]
+*/
+static int ts_data_hdr_read(struct _tsr_pm *pm, ffstr data)
+{
+	const ffbyte *d = (ffbyte*)data.ptr, *end = (ffbyte*)data.ptr + data.len;
+	if (ffint_be_cpu16_ptr(d) != 0x0001)
+		return 0;
+	d += 2;
+
+	pm->stream_id = *d++;
+
+	pm->pkt_len = ffint_be_cpu16_ptr(d);
+	d += 2;
+
+	d++;
+	d++;
+
+	ffuint n = *d++;
+	if (d + n > end || n < 5)
+		return 0;
+	// HI[8] MID[16] LO[16] -> HI[3]MID[15]LO[15]
+	// ....hhh. mmmmmmmmmmmmmmm. lllllllllllllll. -> hhhmmmmmmmmmmmmmmmlllllllllllllll
+	ffuint64 pos = ((ffuint64)(*d & 0x0e) << 29)
+		| ((ffint_be_cpu16_ptr(d + 1) & 0xfffe) << 14)
+		| (ffint_be_cpu16_ptr(d + 3) >> 1);
+	pm->pos_msec = pos / 90;
+	d += n;
+
+	return d - (ffbyte*)data.ptr;
 }
--- avpack/ts-read.h
+++ avpack/ts-read.h
@@ -17,20 +17,6 @@ tsread_error
 #include <ffbase/stream.h>
 #include <ffbase/map.h>
 
-enum _TSR_PMT {
-	_TSR_PMT_TOP,
-	_TSR_PMT_INFO,
-	_TSR_PMT_DATA,
-};
-
-struct _tsr_pm {
-	ffuint pid;
-	ffuint type; // enum _TSR_PMT
-	ffuint stream_type; // enum TS_STREAM
-	ffuint pkt_len;
-	ffuint64 pos_msec;
-};
-
 typedef void (*ts_log_t)(void *udata, const char *fmt, va_list va);
 
 typedef struct tsread {
@@ -42,6 +28,7 @@ typedef struct tsread {
 	ffstream stream;
 	struct ts_packet pkt;
 	const char *error;
+	ffuint sync :1;
 
 	ts_log_t log;
 	void *udata;
@@ -127,122 +114,6 @@ static inline const char* tsread_error(tsread *t)
 	return t->error;
 }
 
-/* Format:
-[8]
-sid[2]
-pid_info[2]
-*/
-static int _tsr_top_read(tsread *t, ffstr data)
-{
-	const ffbyte *d = (ffbyte*)data.ptr;
-	d += 8;
-	d += 2;
-
-	ffuint pid = ffint_be_cpu16_ptr(d) & 0x1fff;
-	d += 2;
-	struct _tsr_pm *pm = _tsr_pm_add(t, pid);
-	pm->type = _TSR_PMT_INFO;
-
-	_tsr_log(t, " pid_info:%xu"
-		, pid);
-	return 0;
-}
-
-/* Format:
-[1]
-[2]
-sid[2]
-[3]
-pid_data[2]
-len[2]
-	...
-stream_type
-pid[2]
-desc_list_len[2]
-tags {
-	desc_tag
-	len
-	data[]
-}...
-*/
-static int _tsr_info_read(tsread *t, ffstr data)
-{
-	const ffbyte *d = (ffbyte*)data.ptr, *end = (ffbyte*)data.ptr + data.len;
-	d += 1;
-	d += 2;
-	d += 2;
-	d += 3;
-
-	ffuint pid = ffint_be_cpu16_ptr(d) & 0x1fff;
-	d += 2;
-	struct _tsr_pm *pm = _tsr_pm_add(t, pid);
-	pm->type = _TSR_PMT_DATA;
-
-	ffuint n = ffint_be_cpu16_ptr(d) & 0x0fff;
-	d += 2;
-	if (n != 0) {
-		t->error = "case not supported";
-		return -1;
-	}
-
-	if (~0U == (pm->stream_type = ts_read8(&d, end)))
-		goto bad;
-
-	_tsr_log(t, " pid_data:%xu  stream_type:%xu"
-		, pm->pid, pm->stream_type);
-	return 0;
-
-bad:
-	t->error = "bad data";
-	return -1;
-}
-
-/* Format:
-header[2] // 0x0001
-stream_id
-packet_len[2]_tsr_data_hdr_read
-[1]
-flags
-pos_len
-pos[]
-*/
-static int _tsr_data_hdr_read(tsread *t, struct _tsr_pm *pm, ffstr *data)
-{
-	const ffbyte *d = (ffbyte*)data->ptr, *end = (ffbyte*)data->ptr + data->len;
-	if (ffint_be_cpu16_ptr(d) != 0x0001)
-		goto bad;
-	d += 2;
-
-	ffuint stream_id = *d++;
-
-	pm->pkt_len = ffint_be_cpu16_ptr(d);
-	d += 2;
-
-	d++;
-	d++;
-
-	ffuint n = *d++;
-	if (d + n > end
-		|| n < 5)
-		goto bad;
-	// HI[8] MID[16] LO[16] -> HI[3]MID[15]LO[15]
-	ffuint64 pos = (((ffuint64)(*d & 0x0e) >> 1) << 30)
-		| ((ffint_be_cpu16_ptr(d + 1) >> 1) << 15)
-		| (ffint_be_cpu16_ptr(d + 3) >> 1);
-	pm->pos_msec = pos / 90;
-	d += n;
-
-	ffstr_set(data, d, end - d);
-
-	_tsr_log(t, " stream_id:%xu  packet:%u  pos:%U"
-		, stream_id, pm->pkt_len, pm->pos_msec);
-	return 0;
-
-bad:
-	t->error = "bad data";
-	return -1;
-}
-
 /** Process .ts data chunk.
 Return enum TSREAD_R */
 static inline int tsread_process(tsread *t, ffstr *input, ffstr *output)
@@ -256,16 +127,32 @@ static inline int tsread_process(tsread *t, ffstr *input, ffstr *output)
 		t->off += r;
 		if (chunk.len < t->gather)
 			return TSREAD_MORE;
+
+		if (!t->sync) {
+			r = ffstr_findchar(&chunk, 0x47);
+			if (r < 0) {
+				ffstream_reset(&t->stream);
+				continue;
+			} else if (r > 0) {
+				ffstream_consume(&t->stream, r);
+				continue;
+			}
+		}
 		chunk.len = t->gather;
 
 		ffmem_zero_obj(&t->pkt);
 		r = ts_pkt_read(&t->pkt, chunk.ptr, chunk.len);
-		t->pkt.off = t->off - ffstream_used(&t->stream);
-		ffstream_consume(&t->stream, t->gather);
 		if (r <= 0) {
-			t->error = "bad header";
+			ffstream_consume(&t->stream, 1);
+			if (!t->sync)
+				continue;
+			t->sync = 0;
+			t->error = "lost synchronization";
 			return TSREAD_WARN;
 		}
+		t->sync = 1;
+		t->pkt.off = t->off - ffstream_used(&t->stream);
+		ffstream_consume(&t->stream, t->gather);
 
 		const struct ts_packet *p = &t->pkt;
 		_tsr_log(t, "packet #%u: pid:%xu  counter:%u  adaptation:%u(%xu)  start:%u  payload:%u  offset:%xU"
@@ -280,20 +167,35 @@ static inline int tsread_process(tsread *t, ffstr *input, ffstr *output)
 
 		switch (pm->type) {
 		case _TSR_PMT_TOP:
-			if (_tsr_top_read(t, t->pkt.body))
+			if ((r = ts_top_read(t->pkt.body)) <= 0)
 				return TSREAD_ERROR;
+			pm = _tsr_pm_add(t, r);
+			pm->type = _TSR_PMT_INFO;
+			_tsr_log(t, " pid_info:%xu"
+				, pm->pid);
 			break;
 
-		case _TSR_PMT_INFO:
-			if (_tsr_info_read(t, t->pkt.body))
+		case _TSR_PMT_INFO: {
+			ffuint stream_type;
+			if ((r = ts_info_read(t->pkt.body, &stream_type)) <= 0) {
+				t->error = "bad info block";
 				return TSREAD_ERROR;
+			}
+			pm = _tsr_pm_add(t, r);
+			pm->type = _TSR_PMT_DATA;
+			pm->stream_type = stream_type;
+			_tsr_log(t, " pid_data:%xu  stream_type:%xu"
+				, pm->pid, pm->stream_type);
 			break;
+		}
 
 		case _TSR_PMT_DATA:
 			if (t->pkt.start) {
-				if (_tsr_data_hdr_read(t, pm, &t->pkt.body))
+				if (0 == (r = ts_data_hdr_read(pm, t->pkt.body)))
 					return TSREAD_ERROR;
-				pm->pos_msec = t->pkt.pos_msec;
+				_tsr_log(t, " stream_id:%xu  packet:%u  pos:%U"
+					, pm->stream_id, pm->pkt_len, pm->pos_msec);
+				ffstr_shift(&t->pkt.body, r);
 			}
 
 			*output = t->pkt.body;
